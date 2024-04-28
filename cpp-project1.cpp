b0dac31a4972b31a2076b104014ef4ef6584deac
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-04-28 23:17:29 +0530

    Update cpp-project1.cpp

--- cpp-project1.cpp
+++ cpp-project1.cpp
@@ -6,26 +6,6 @@ using namespace std;
 
 
 
-class account
-{
-	int acno;
-	char name[50];
-	int deposit;
-	char type;
-public:
-	void create_account();	
-	void show_account() const;
-	void modify();	
-	void dep(int);	
-	void draw(int);	
-	void report() const;	
-	int retacno() const;	
-	int retdeposit() const;	
-	char rettype() const;	
-};        
-
-
-
 
 
 
@@ -51,24 +31,6 @@ public:
 
 
 
-
-class account
-{
-	int acno;
-	char name[50];
-	int deposit;
-	char type;
-public:
-	void create_account();	
-	void show_account() const;
-	void modify();	
-	void dep(int);	
-	void draw(int);	
-	void report() const;	
-	int retacno() const;	
-	int retdeposit() const;	
-	char rettype() const;	
-};        
 
 
 
