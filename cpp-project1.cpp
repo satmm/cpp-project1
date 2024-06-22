30120c9cc7bc567911e938494af0ce55d0d1cc2c
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-06-22 22:32:57 +0530

    Update cpp-project1.cpp

--- cpp-project1.cpp
+++ cpp-project1.cpp
@@ -51,7 +51,19 @@ void account::show_account() const
 
 
 
-
+//modify the details
+void account::modify()
+{
+	cout<<"\nAccount No. : "<<acno;
+	cout<<"\nModify Account Holder Name : ";
+	cin.ignore();
+	cin.getline(name,50);
+	cout<<"\nModify Type of Account : ";
+	cin>>type;
+	type=toupper(type);
+	cout<<"\nModify Balance amount : ";
+	cin>>deposit;
+}
 
 
 
