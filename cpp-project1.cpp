d0a21d71a8b6d09954e07f48e8550338f273f583
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-08-14 23:13:15 +0530

    Update cpp-project1.cpp

--- cpp-project1.cpp
+++ cpp-project1.cpp
@@ -4,16 +4,6 @@
 #include<iomanip>
 using namespace std;
 
-
-#include<iostream>
-#include<fstream>
-#include<cctype>
-#include<iomanip>
-using namespace std;
-
-
-
-
 #include<iostream>
 #include<fstream>
 #include<cctype>
