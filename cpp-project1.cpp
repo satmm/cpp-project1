c8b716c4c310dc8ed4f896ba231368d9269eda1f
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-08-17 22:26:55 +0530

    Update cpp-project1.cpp

--- cpp-project1.cpp
+++ cpp-project1.cpp
@@ -18,12 +18,6 @@ using namespace std;
 #include<iomanip>
 using namespace std;
 
-#include<iostream>
-#include<fstream>
-#include<cctype>
-#include<iomanip>
-using namespace std;
-
 
 
 // Create Account
