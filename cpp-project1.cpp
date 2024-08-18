be853ba396bcfd2bb56b78ecd35eeab2147e42d7
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-08-18 22:40:28 +0530

    Update cpp-project1.cpp

--- cpp-project1.cpp
+++ cpp-project1.cpp
@@ -37,14 +37,6 @@ using namespace std;
 
 
 
-#include<iostream>
-#include<fstream>
-#include<cctype>
-#include<iomanip>
-using namespace std;
-
-
-
 // Create Account
 void account::create_account()
 {
