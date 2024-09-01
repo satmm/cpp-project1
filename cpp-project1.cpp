ffdb117521bf65a2c91cdee8bd0846f781391986
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-09-01 22:22:32 +0530

    Update cpp-project1.cpp

--- cpp-project1.cpp
+++ cpp-project1.cpp
@@ -16,17 +16,6 @@ using namespace std;
 
 
 
-
-
-#include<iostream>
-#include<fstream>
-#include<cctype>
-#include<iomanip>
-using namespace std;
-
-
-
-
 #include<iostream>
 #include<fstream>
 #include<cctype>
